28bebd46d56094a7662ab3fe07ba91422ba11ae3
philippspo <philipp.sporrer@planifica.de>
2015-08-13 23:53:22 +0200

    added user information to messages publication
    so that the data of the current user and the partner user is published
    with the message

--- server/publications/messages.js
+++ server/publications/messages.js
@@ -2,16 +2,25 @@ Meteor.publish('messages', function (partnerId) {
     if (!partnerId) {
         return;
     }
-    return Messages.find({
-        chatPartner: {
-            $in: [
-                partnerId, this.userId
-            ]
-        },
-        author: {
-            $in: [
-                partnerId, this.userId
-            ]
-        }
-    });
+    return [
+        Messages.find({
+            chatPartner: {
+                $in: [
+                    partnerId, this.userId
+                ]
+            },
+            author: {
+                $in: [
+                    partnerId, this.userId
+                ]
+            }
+        }),
+        Meteor.users.find({
+            _id: {
+                $in: [
+                    partnerId, this.userId
+                ]
+            }
+        })
+    ];
 });
